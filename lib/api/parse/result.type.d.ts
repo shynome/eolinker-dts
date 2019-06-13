76d8b0415b324e6274c3ab85fcb3c3d1b99ac456
shynome <shynome@gmail.com>
2019-06-14 00:13:43 +0800

    参数定义移动到 .d.ts 文件中

---
+++ lib/api/parse/result.type.d.ts
@@ -0,0 +1,22 @@
+
+export interface ResultParamValue {
+  value           : string
+  valueDescription: string
+}
+
+export interface ResultParam {
+  paramID         : number
+  paramName       : string
+  paramKey        : string
+  paramNotNull    : number
+  paramOptionValue: ResultParamValue[]
+  apiID           : number
+}
+
+export interface ResultRule {
+  $index    : number;
+  paramKey  : string;
+  paramType : string;
+  paramValue: string;
+  type      : string;
+}
